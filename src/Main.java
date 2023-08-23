9ca6a1512af92462519ae6dc8354c8bbf3d03997
Anastasia Anisimova <pin_up@bk.ru>
2023-08-23 23:17:48 +0300

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Семён";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        //fullName = fullName.replace("ё", "е");
+        System.out.println("ФИО сотрудника: " + fullName);
+
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + upperFullName);
+
+        if(fullName.contains("ё")) {
+            fullName = fullName.replace("ё", "е");
+            System.out.println("Данные ФИО сотрудника - " +fullName);
+        }
+
+
+
 
     }
 }
